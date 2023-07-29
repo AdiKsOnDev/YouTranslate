c23136daecf5c29bdddb0edcc3818626c89d5cf6
AdiKsOnDev <adilalizade13@gmail.com>
2023-07-30 02:03:35 +0400

    Added comments and docstrings

--- FlaskAPIRouting.py
+++ FlaskAPIRouting.py
@@ -1,34 +1,31 @@
 from flask import Flask, request, jsonify
+
 from pytube import YouTube
-import helperFunctions as hf
-import voiceCloning as vc
+from helperFunctions import *
+from voiceCloning import *
+
 app = Flask(__name__)
 
+@app.route('/translated_audio', methods=['POST'])
+def translated_audio():
+    """ An API route that sends back the audio track
 
-# API Routes
-"""
-    Mini-Route for just audio
-"""
-@app.route('/translated_voice_audio', methods=['POST'])
-def translated_voice_audio():
+        Returns:
+            bytes: The .mp3 file
+            response: The HTTPS response code (Depending on the outcome)
+    """    
     data = request.get_json()
     video_url = data['video_url']
     language_code = data['language_code']
 
     try:
-        yt = YouTube(video_url)
-    except Exception as e:
-        return jsonify({"error": f"Connection Error: {e}"}), 400
-
-    try:
-        
         video_id = video_url.split('=')[1]
-        hf.get_transcript(video_id, "data/transcript.txt")
-        hf.get_translation("data/transcript.txt", "data/translated_transcript.txt", language_code)
+        get_transcript(video_id, "data/transcript.txt")
+        get_translation("data/transcript.txt", "data/translated_transcript.txt", language_code)
 
         with open("data/translated_transcript.txt", "r",  encoding="utf-8") as file:
-            vc.voiceOver(file.read(), "audios/VoiceOver.mp3")
-            vc.delete_cloned_voice()
+            voiceOver(file.read(), "audios/VoiceOver.mp3")
+            delete_cloned_voice()
 
     except Exception as e:
         return jsonify({"error": f"Error generating voice audio: {e}"}), 500
@@ -39,12 +36,14 @@ def translated_voice_audio():
 
     return audio_data, 200, {'Content-Type': 'audio/mpeg'}
 
-
-"""
-    complete route with video
-"""
 @app.route('/translated_with_video', methods=['POST'])
-def translate_and_video():
+def translated_with_video():
+    """ An API route for the final video with the translated audio track
+
+    Returns:
+        bytes: The .mp4 file
+        response: The HTTPS response code (Depending on the outcome)
+    """    
     data = request.get_json()
     video_url = data['video_url']
     language_code = data['language_code']
@@ -55,19 +54,26 @@ def translate_and_video():
         return jsonify({"error": f"Connection Error: {e}"}), 400
 
     try:
-        yt.streams.filter(progressive=True, file_extension="mp4").first().download(output_path="videos", filename="Original_Video")
+        yt.streams.filter(
+            progressive=True, 
+            file_extension="mp4"
+        ).first().download(
+            output_path="videos", 
+            filename="Original_Video"
+        )
         video_id = yt.video_id
-        hf.get_transcript(video_id, "data/transcript.txt")
-        hf.get_translation("data/transcript.txt", "data/translated_transcript.txt", language_code)
-        hf.extract_audio("videos/Original_Video", "audios/sample.mp3")
+
+        get_transcript(video_id, "data/transcript.txt")
+        get_translation("data/transcript.txt", "data/translated_transcript.txt", language_code)
+        extract_audio("videos/Original_Video", "audios/sample.mp3")
 
         with open("data/translated_transcript.txt", "r") as file:
-            vc.voiceOver(file.read(), "audios/VoiceOver.mp3")
-            vc.delete_cloned_voice()
+            voiceOver(file.read(), "audios/VoiceOver.mp3")
+            delete_cloned_voice()
 
-        hf.replace_audio("videos/Original_Video", "audios/VoiceOver.mp3", "videos/Final_Video.mp4")
+        replace_audio("videos/Original_Video", "audios/VoiceOver.mp3", "videos/Final_Video.mp4")
     except Exception as e:
-        return jsonify({"error": f"Error translating and creating video: {e}"}), 500
+        return jsonify({"Error": f"Error translating and creating video: {e}"}), 500
 
     # Read the final video and send it as a response
     with open("videos/Final_Video.mp4", "rb") as file:
